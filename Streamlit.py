fcd39dfe5141b0f237beea94ee2c145c5dca0dad
95abhijeet <92116565+95abhijeet@users.noreply.github.com>
2022-03-04 15:55:58 -0800

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as  components
+
 header = st.container()
 
 
 with header:
     st.title('Covid impact on labour market')
 
-    htmlfile=open("F:/Second term/5000/Project/Google trend.html", 'r', encoding ='utf-8')
+    htmlfile=open("Google trend.html", 'r', encoding ='utf-8')
     source_code = htmlfile.read()
     print(source_code)
-    components.html(source_code, height = 1000, width = 1000)
+    components.html(source_code, height = 800, width = 1000)
